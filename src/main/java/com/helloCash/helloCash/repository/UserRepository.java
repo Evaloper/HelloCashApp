fc4bfca81ef751f7c57d1c2883ae4b01b2cb12a4
evaloper <gbajeeva@yahoo.com>
2024-06-17 00:15:03 +0100

    Save User's Phone number and Pin to database

--- src/main/java/com/helloCash/helloCash/repository/UserRepository.java
+++ src/main/java/com/helloCash/helloCash/repository/UserRepository.java
@@ -1,20 +1,10 @@
 package com.helloCash.helloCash.repository;
 
 import com.helloCash.helloCash.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
-public class UserRepository {
-    private final Map<String, UserEntity> users = new HashMap<>();
-
-    public void save(UserEntity user) {
-        users.put(user.getPhoneNumber(), user);
-    }
-
-    public UserEntity findByPhoneNumber(String phoneNumber) {
-        return users.get(phoneNumber);
-    }
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }
